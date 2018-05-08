b5af27592985d7fa1f64fb54b7ac80148ab3d44b
Renoj Varghese <renoj.varghese@uconn.edu>
2018-05-08 15:11:26 -0400

    can add items to list

--- src/List.js
+++ src/List.js
@@ -3,18 +3,39 @@ import React, { Component } from 'react';
 export class List extends Component {
     constructor(props) {
         super(props);
-        this.items = ["make a React App", "design a logo"];
+        this.state = {
+            items : []
+        }
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
     get list() {
-        return this.items.map(item => {
-            return <li className="item">{item}</li>
+        return this.state.items.map( (item, i) => {
+            return <li key={"item_" + i} className="item">{item}</li>
         })
+    }
+    handleSubmit() {
+        let inputElem = document.getElementById("text-input");
+        let newItem = inputElem.value;
+
+        console.log(newItem);
+        this.state.items.push(newItem);
+        this.setState({
+            items: this.state.items
+        });
+
+        inputElem.value = "";
+
+
     }
     render() {
         return (
-            <ul className="item-list">
-                {this.list}
-            </ul>
+            <div className="container">
+                <ul className="item-list">
+                    {this.list}
+                </ul>
+                <input type="text" id="text-input"/>
+                <button className="submit-btn" onClick={this.handleSubmit}>Submit</button>
+            </div>
         );
     }
 }
