a90ea2af7dd8bb66414031530f185032bcddd631
Renoj Varghese <renoj.varghese@uconn.edu>
2018-05-08 14:19:58 -0400

    list can render

--- src/App.js
+++ src/App.js
@@ -3,19 +3,24 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
+    constructor(props) {
+        super(props);
+        this.items = ["make a React App", "design a logo"];
+    }
+    get list() {
+        return this.items.map(item => {
+            return <li>{item}</li>
+        })
+    }
+    render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <ul className="item-list">
+            {this.list}
+        </ul>
       </div>
     );
-  }
+    }
 }
 
 export default App;
